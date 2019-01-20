7a49a19b22c15c2c0da7e2cd4d6aa0ef94eac5d2
Dmitry <freddy16.91@mail.ru>
2019-01-20 15:42:02 +0300

    Задачка на возведение числа в куб

---
+++ ItAcademy/src/Practice/ex_1_func/ex_7.java
@@ -0,0 +1,17 @@
+package Practice.ex_1_func;
+
+import java.util.Scanner;
+
+/*
+7. Написать функцию, которая принимает один целочисленный параметр и возвращает значение,
+равное этому параметру, возведенному в куб.
+ */
+public class ex_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = sc.nextInt();
+
+        System.out.println(number * number * number);
+    }
+}
