bb70e64a919eefa314816f8c71cb6d22b26fc63b
Dmitry <freddy16.91@mail.ru>
2019-01-20 15:33:52 +0300

    Задачка на наибольшее число

---
+++ ItAcademy/src/Practice/ex_1_func/ex_6.java
@@ -0,0 +1,22 @@
+package Practice.ex_1_func;
+
+import java.util.Scanner;
+
+/*
+6. Написать функцию, которая принимает 2 целочисленных параметра, и возвращает наибольший из них.
+ */
+public class ex_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 2-а числа для сравнения: ");
+        int first_number = sc.nextInt();
+        int second_number = sc.nextInt();
+
+        if (first_number > second_number) {
+            System.out.println("Наибольшее из них - " + first_number);
+        } else {
+            System.out.println("Наибольшее из них - " + second_number);
+        }
+
+    }
+}
