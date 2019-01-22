d4fe53c6af7ed3bb2b5bdb0666a96a7c700f76d8
Dmitry <freddy16.91@mail.ru>
2019-01-22 23:35:54 +0300

    с помощью цикла

---
+++ ItAcademy/src/hw_2_iter/ex_2_cycle.java
@@ -0,0 +1,27 @@
+package hw_2_iter;
+
+import java.util.Scanner;
+/*
+2.1. Написать программу, вычисляющую и отображающую все числа Фибоначчи меньше введённого пользователем целого числа.
+Цикл
+ */
+public class ex_2_cycle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = sc.nextInt();
+        fibonacci(number);
+
+    }
+    private static void fibonacci(int number){
+        int ft1 = 0;
+        int sc2 = 1;
+        int summFibonacci;
+        for (int i = 0; i < number; i++){
+            summFibonacci = ft1 + sc2;
+            ft1 = sc2;
+            sc2 = summFibonacci;
+            System.out.print(summFibonacci + " ");
+        }
+    }
+}
