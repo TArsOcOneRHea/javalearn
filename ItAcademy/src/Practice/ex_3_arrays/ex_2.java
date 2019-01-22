cb416035166636bd3d43a1a20564451cd42e7ca4
Dmitry <freddy16.91@mail.ru>
2019-01-22 21:57:47 +0300

    Напишите программу, заносящую в массив первые 100 натуральных чисел, делящихся на 13 или на 17, и печатающую его.

--- ItAcademy/src/Practice/ex_3_arrays/ex_2.java
+++ ItAcademy/src/Practice/ex_3_arrays/ex_2.java
@@ -6,18 +6,19 @@ package Practice.ex_3_arrays;
 public class ex_2 {
     public static void main(String[] args) {
         int[] array = new int[100];
+        proverka(array);
     }
 
-    static void proverka(int[] array) {
+    private static void proverka(int[] array) {
         int i = 0;
         int number = 1;
         while (i != array.length) {
             if ((number % 13 == 0) || (number % 17 == 0)) {
                 array[i] = number;
+                i++;
+                System.out.print(number + " ");
             }
             number++;
         }
-
-
     }
 }
