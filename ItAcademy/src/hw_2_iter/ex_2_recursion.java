e55f7de948f3ea8d916d7f602260a52d50a0b0d8
Dmitry <freddy16.91@mail.ru>
2019-01-22 23:51:39 +0300

    с помощью рекурсии

---
+++ ItAcademy/src/hw_2_iter/ex_2_recursion.java
@@ -0,0 +1,23 @@
+package hw_2_iter;
+
+import java.util.Scanner;
+
+/*
+2.1. Написать программу, вычисляющую и отображающую все числа Фибоначчи меньше введённого пользователем целого числа.
+Рекурсия
+ */
+public class ex_2_recursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = sc.nextInt();
+        fibonacci(number);
+    }
+
+    protected static fibonacci(int number) {
+        if (number == 1) return 1;
+        if (number == 2) return 1;
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
+}
+
