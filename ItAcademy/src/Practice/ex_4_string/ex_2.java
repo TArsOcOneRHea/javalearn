1d1eeeaca393a9820c435f739c49a9cf88c87ea3
user <freddy16.91@mail.ru>
2019-01-23 19:56:06 +0300

    ex_2

--- ItAcademy/src/Practice/ex_4_string/ex_2.java
+++ ItAcademy/src/Practice/ex_4_string/ex_2.java
@@ -2,9 +2,14 @@ package Practice.ex_4_string;
 
 public class ex_2 {
     public static void main(String[] args) {
-        String string = ":(jihhuhuhu:)kjlkjljhkl:(hjhuby(";
-        System.out.println(string);
-        string = string.replaceAll( ":\\(" , ":)");
-        System.out.println(string);
+
+        String repl = repl(":(");
+        System.out.println(repl.equals(":)"));
+        System.out.println(repl("").equals(""));
+        System.out.println(repl(":)").equals(":)"));
+    }
+
+    private static String repl(String string) {
+        return string.replaceAll( ":\\(" , ":)");
     }
 }
