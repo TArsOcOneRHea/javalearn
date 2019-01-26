472327da30f73583cbfe01c6598445517e053ca3
Dmitry <freddy16.91@mail.ru>
2019-01-26 21:59:59 +0300

    //

---
+++ ItAcademy/src/Homework/hw_1_func/ex_2.java
@@ -0,0 +1,47 @@
+package Homework.hw_1_func;
+
+import java.util.Scanner;
+
+/*
+Имеются два дома размерами a на b и c на d. Размеры вводятся польователем с консоли.
+Проверить, помещаются ли эти дома на участке размерами e на f.
+Стороны домов - параллельны сторонам участка, в остальном размещение может быть любым.
+ */
+public class ex_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Разсмер первого дома:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Разсмер второго дома:");
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.println("Разсмер участка:");
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+
+        checkTheCapacityOfHouses(a, b, c, d, e, f);
+    }
+
+    private static void checkTheCapacityOfHouses(int a, int b, int c, int d, int e, int f) {
+        if((e >= (a + c)) && (f >= b && f >= d)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((e >= (a + d)) && (f >= c && f >= b)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((e >= (b + c)) && (f >= a && f >= d)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((e >= (b + d)) && (f >= a && f >= c)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((f >= (a + c)) && (e >= b && e >= d)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((f >= (a + d)) && (e >= c && e >= b)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((f >= (b + c)) && (e >= a && e >= d)) {
+            System.out.println("Можно построить 2 дома");
+        } else if ((f >= (b + d)) && (e >= a && e >= c)) {
+            System.out.println("Можно построить 2 дома");
+        } else
+            System.out.println("Нельзя построить 2 дома");
+    }
+}
--- ItAcademy/src/Practice/_2_oop/ex_1_class_and_encapsulation/Time.java
+++ ItAcademy/src/Practice/_2_oop/ex_1_class_and_encapsulation/Time.java
@@ -1,14 +1,14 @@
-package Practice._2_oop;
+package Practice._2_oop.ex_1_class_and_encapsulation;
 
 public class Time {
-    int seconds;
+    int seconds = 3700;
     int minutes;
     int hours;
 
     public Time(int allSeconds){
-        int sec = seconds % 3600;
-        int min = (seconds / 60) % 60;
-        int hrs = seconds / 3600;
+        int sec = seconds % 60;
+        int min = (seconds - sec) / 60;
+        int hrs = ((seconds - sec) - min) / 60;
     }
 
     public Time(int seconds, int minutes, int hours) {
