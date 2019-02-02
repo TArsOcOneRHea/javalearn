613f350c23ee770507dd7a2d113eb535c85b4a3a
Dmitry <freddy16.91@mail.ru>
2019-02-02 23:28:38 +0300

    //

--- ItAcademy/src/Practice/_1_procedural/ex_2_iter/ex_1.java
+++ ItAcademy/src/Practice/_1_procedural/ex_2_iter/ex_1.java
@@ -1,5 +1,9 @@
 package Practice._1_procedural.ex_2_iter;
-
+/*
+В 1626 году индейцы продали Манхэттен за 24$.
+Написать программу, которая высчитывает сумму, получившуюся бы в текущем году, если бы индейцы положили
+эти деньги в банк под 5% годовых.
+ */
 public class ex_1 {
     public static void main(String[] args) {
         double money = 24d;
