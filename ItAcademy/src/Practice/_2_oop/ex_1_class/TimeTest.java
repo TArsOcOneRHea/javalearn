9d5b05691adc9d94b8087d16a0d05687b8a0e613
Dmitry <freddy16.91@mail.ru>
2019-02-02 16:10:29 +0300

    //

--- ItAcademy/src/Practice/_2_oop/ex_1_class/TimeTest.java
+++ ItAcademy/src/Practice/_2_oop/ex_1_class/TimeTest.java
@@ -2,7 +2,7 @@ package _2_oop.ex_1_class;
 
 public class TimeTest {
     public static void main(String[] args) {
-        Time time = new Time(3758978, 101389, 356);
+        Time time = new Time(3758978, 101356989, 356);
         Time time1 = new Time(37056860);
         System.out.println(time);
         System.out.println(time1);
