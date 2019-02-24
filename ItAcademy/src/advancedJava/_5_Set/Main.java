a2fcbbb66365dbcafff1113fdefe541637836fa1
Dmitry <freddy16.91@mail.ru>
2019-02-24 12:31:36 +0300

    //

--- ItAcademy/src/advancedJava/_5_Set/Main.java
+++ ItAcademy/src/advancedJava/_5_Set/Main.java
@@ -50,5 +50,42 @@ public class Main {
 
         System.out.println(hashSet.isEmpty());
 
+        //________________union________________
+        Set<Integer> set1 = new HashSet<>();
+
+        set1.add(0);
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+        set1.add(6);
+
+        Set<Integer> set2 = new HashSet<>();
+
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+        set2.add(8);
+
+        //____union - объединение массивов_____
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+        System.out.println(union);
+
+        //_intersection - пересечение множеств_
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println(intersection);
+
+        //____difference - зазность множеств___
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        System.out.println(difference);
+
+
     }
 }
