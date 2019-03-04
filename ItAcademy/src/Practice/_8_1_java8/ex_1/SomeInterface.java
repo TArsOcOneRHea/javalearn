2922ec7dc16601372699536ae8dd4852120529ef
Apelsin <tre21jfk$>
2019-03-04 21:16:13 +0300

    //

---
+++ ItAcademy/src/Practice/_8_1_java8/ex_1/SomeInterface.java
@@ -0,0 +1,10 @@
+package Practice._8_1_java8.ex_1;
+
+/**
+ * Created by user on 04.03.2019.
+ */
+@FunctionalInterface
+public interface SomeInterface<T>{
+    boolean test(T rate1, T rate2, T rate3);
+
+}
