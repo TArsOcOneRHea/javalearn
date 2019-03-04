4250ca3ad69213b93e5d778e8899af2c8730940b
Apelsin <tre21jfk$>
2019-03-04 21:20:35 +0300

    //

---
+++ ItAcademy/src/Practice/_8_1_java8/ex_1/ex_2/Main.java
@@ -0,0 +1,7 @@
+package Practice._8_1_java8.ex_1.ex_2;
+
+/**
+ * Created by user on 04.03.2019.
+ */
+public class Main {
+}
---
+++ ItAcademy/src/Practice/_8_1_java8/ex_1/ex_2/Singer.java
@@ -0,0 +1,14 @@
+package Practice._8_1_java8.ex_1.ex_2;
+
+/**
+ * Created by user on 04.03.2019.
+ */
+public class Singer {
+    final String name;
+    final String homeTown;
+
+    public Singer(String name, String homeTown) {
+        this.name = name;
+        this.homeTown = homeTown;
+    }
+}
