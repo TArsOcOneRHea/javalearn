225f070c029758fd2bb11be0172aae08581f910f
nikitashidlovskiy <retdarkww@gmail.com>
2019-03-11 20:35:30 +0300

    //

---
+++ ItAcademy/src/Practice/_8_patterns/Main.java
@@ -0,0 +1,10 @@
+package Practice._8_patterns;
+
+/**
+ * Created by user on 11.03.2019.
+ */
+public class Main {
+    public static void main(String[] args) {
+        User.Builder builder = User.builder().name("Nick").age(7);
+    }
+}
