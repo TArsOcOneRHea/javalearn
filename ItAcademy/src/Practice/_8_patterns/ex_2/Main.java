d9c095903e141f4162e62d97be6e3b20b52f5fc2
nikitashidlovskiy <retdarkww@gmail.com>
2019-03-13 20:48:36 +0300

    //

--- ItAcademy/src/Practice/_8_patterns/ex_2/Main.java
+++ ItAcademy/src/Practice/_8_patterns/ex_2/Main.java
@@ -7,9 +7,12 @@ import java.util.Arrays;
  */
 public class Main {
     public static void main(String[] args) {
-        Animal dog1 = new Animal("Rex", null);
-        Animal dog2 = new Animal("Bender", null);
-        Animal dog3 = new Animal("Fry", null);
+        User user1 = new User(1, "Daniel");
+        User user2 = new User(2, "Chack");
+        User user3 = new User(3, "Tom");
+        Animal dog1 = new Animal("Rex", user1);
+        Animal dog2 = new Animal("Bender", user2);
+        Animal dog3 = new Animal("Fry", user3);
         AnimalManager animalManager = new AnimalManager(Arrays.asList(dog1, dog2, dog3));
         animalManager.forEach(new SitDown());
         animalManager.forEach(new Bark());
