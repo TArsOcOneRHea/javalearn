87999326e06b0057b3c2a486923ebff926c8248f
Dmitry <freddy16.91@mail.ru>
2019-03-17 12:35:30 +0300

    //

---
+++ ItAcademy/src/ITVDN/Main.java
@@ -0,0 +1,28 @@
+package ITVDN;
+
+public class Main {
+    static byte by;
+    static short s;
+    static int i;
+    static long l;
+
+    static float f;
+    static double d;
+
+    static boolean b;
+    static char c;
+
+    public static void main(String[] args) {
+        byte counter;
+        System.out.println(by);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+
+        System.out.println(f);
+        System.out.println(d);
+
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
